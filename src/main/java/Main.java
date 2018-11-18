34f62ff23e3b44d223a12dc5180993b487b79e24
sviridov-n-i <sviridov.n.i.vrn@gmail.com>
2018-11-18 22:27:44 +0300

    commit directory

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,14 @@ public class Main {
     public static void f(){
         System.out.println("some bag");
     }
+    public static void g(){
+        System.out.println("some bag");
+    }
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
         f();
+        g();
+
     }
 }
