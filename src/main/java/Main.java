dff5a25fa7014b3431a624717a1a91c2d13f0d22
sviridov-n-i <sviridov.n.i.vrn@gmail.com>
2018-11-18 20:07:57 +0300

    add bug

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,11 @@
 public class Main {
 
+    public static void f(){
+        System.out.println("some bag");
+    }
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
-
+        f();
     }
 }
